f694c556616f102be205b0f94562d4a0ebac214e
lindenmp <lindenparkes@gmail.com>
2024-04-24 10:57:35 -0400

    simplified code structure and updated for new optimization code

--- scripts/compute_optimized_control_energy.py
+++ scripts/compute_optimized_control_energy.py
@@ -33,166 +33,84 @@ def run(config):
     A_file = config['A_file']
     fmri_clusters_file = config['fmri_clusters_file']
     file_prefix = config['file_prefix']
-    time_horizon = config['time_horizon']
-    c = config['c']
-    rho = config['rho']
 
     # load A matrix
     adjacency = np.load(os.path.join(indir, A_file))
-    system = 'continuous'
-    adjacency_norm = matrix_normalization(adjacency, system=system, c=c)
 
     # load rsfMRI clusters
     fmri_clusters = np.load(os.path.join(outdir, fmri_clusters_file), allow_pickle=True).item()
     centroids = fmri_clusters['centroids']
     [n_states, n_nodes] = centroids.shape
+    print('\nbrain state params: k = {0}'.format(n_states))
 
-    print('\nk={0}; time_horizon={1}; c={2}; rho={3}'.format(n_states, time_horizon, c, rho))
+    # nct params
+    system = 'continuous'
+    c = config['c']
+    time_horizon = config['time_horizon']
+    rho = config['rho']    
     control_set = np.eye(n_nodes)  # define control set using a uniform full control set
     trajectory_constraints = np.eye(n_nodes)  # define state trajectory constraints
-    n_steps = 5  # number of gradient steps
-    lr = 0.1  # learning rate for gradient
+    print('nct params: c = {0}; time_horizon = {1}; rho = {2}'.format(c, time_horizon, rho))
 
+    # normalize adjacency matrix
+    adjacency_norm = matrix_normalization(adjacency, system=system, c=c)
+
+    # training params
+    n_steps = config['n_steps']  # number of gradient steps
+    lr = config['lr']  # learning rate for gradient
+    eig_weight = config['eig_weight']  # regularization strength for eigen value penalty
+    reg_weight = config['reg_weight']  # regularization strength for weight penalty (e.g., l2)
+    print('training params: n_steps = {0}; lr = {1}; eig_weight = {2}; reg_weight = {3}'.format(n_steps, lr, eig_weight, reg_weight))
+
+    # compute control energy
     start = time.time()
-    # --- compute control energy. Reference state = target state ---
-    print('Compute control energy. Reference state = target state...')
-    optimized_weights_xf = np.zeros((n_states, n_states, n_steps, n_nodes))
-    control_energy_xf = np.zeros((n_states, n_states))
-    for initial_idx in tqdm(np.arange(n_states)):
-        initial_state = normalize_state(centroids[initial_idx, :])  # initial state
-        for target_idx in np.arange(n_states):
-            if initial_idx != target_idx:
-                target_state = normalize_state(centroids[target_idx, :])  # target state
-                reference_state = target_state
+    log_args = dict()
+    # for reference_state in ['zero',]:
+    for reference_state in ['zero', 'midpoint', 'target_state']:
+        print('\ncomputing control energy. Reference state = {0}...'.format(reference_state))
+        loss = np.zeros((n_states, n_states, n_steps))
+        eigen_values = np.zeros((n_states, n_states, n_steps))
+        optimized_weights = np.zeros((n_states, n_states, n_steps, n_nodes))
+        control_energy = np.zeros((n_states, n_states))
+        
+        for initial_idx in np.arange(n_states):
+            initial_state = normalize_state(centroids[initial_idx, :])  # initial state
+            for target_idx in np.arange(n_states):
+                print('initial_state = {0}, target_state = {1}'.format((initial_state, target_state)))
+                if initial_idx != target_idx:
+                    target_state = normalize_state(centroids[target_idx, :])  # target state
 
-                loss, opt_weights = train_nct(adjacency_norm=adjacency_norm, initial_state=initial_state, target_state=target_state,
-                                              time_horizon=time_horizon, control_set=control_set,
-                                              reference_state=reference_state, rho=rho, trajectory_constraints=trajectory_constraints,
-                                              n_steps=n_steps, lr=lr)
-                optimized_weights_xf[initial_idx, target_idx] = opt_weights
+                    loss[initial_idx, target_idx], \
+                    eigen_values[initial_idx, target_idx], \
+                    optimized_weights[initial_idx, target_idx] = train_nct(adjacency_norm=adjacency_norm, initial_state=initial_state, target_state=target_state,
+                                                                           time_horizon=time_horizon, control_set=control_set,
+                                                                           reference_state=reference_state, rho=rho, trajectory_constraints=trajectory_constraints,
+                                                                           n_steps=n_steps, lr=lr, eig_weight=eig_weight, reg_weight=reg_weight)
 
-                adjacency_norm_opt = adjacency_norm - np.diag(opt_weights[-1, :])  # A_norm with optimized self-inhibition weights
-                if np.any(np.diag(adjacency_norm_opt) > 0):
-                    print('Found {0} positive diagonals on adjacency_norm_opt'.format(np.sum(np.diag(adjacency_norm_opt) > 0)))
+                    idx = np.where(np.isnan(loss[initial_idx, target_idx]))[0][0] - 1
+                    adjacency_norm_opt = adjacency_norm - np.diag(optimized_weights[initial_idx, target_idx, idx, :])  # A_norm with optimized self-inhibition weights
 
-                # get the state trajectory and the control signals
-                state_trajectory, control_signals, numerical_error = get_control_inputs(A_norm=adjacency_norm_opt, x0=initial_state, xf=target_state,
-                                                                                        T=time_horizon, B=control_set,
-                                                                                        xr=reference_state, rho=rho, S=trajectory_constraints,
-                                                                                        system=system)
+                    # get the state trajectory and the control signals
+                    state_trajectory, control_signals, numerical_error = get_control_inputs(A_norm=adjacency_norm_opt, x0=initial_state, xf=target_state,
+                                                                                            T=time_horizon, B=control_set,
+                                                                                            xr=reference_state, rho=rho, S=trajectory_constraints,
+                                                                                            system=system)
 
-                # integrate control signals to get control energy
-                node_energy = integrate_u(control_signals)
-                # summarize nodal energy
-                control_energy_xf[initial_idx, target_idx] = np.sum(node_energy)
-
-    # --- compute control energy. Reference state = midpoint ---
-    print('Compute control energy. Reference state = midpoint...')
-    optimized_weights_midpoint = np.zeros((n_states, n_states, n_steps, n_nodes))
-    control_energy_midpoint = np.zeros((n_states, n_states))
-    reference_state = 'midpoint'  # reference state
-    for initial_idx in tqdm(np.arange(n_states)):
-        initial_state = normalize_state(centroids[initial_idx, :])  # initial state
-        for target_idx in np.arange(n_states):
-            if initial_idx != target_idx:
-                target_state = normalize_state(centroids[target_idx, :])  # target state
-
-                loss, opt_weights = train_nct(adjacency_norm=adjacency_norm, initial_state=initial_state, target_state=target_state,
-                                                    time_horizon=time_horizon, control_set=control_set,
-                                                    reference_state=reference_state, rho=rho, trajectory_constraints=trajectory_constraints,
-                                                    n_steps=n_steps, lr=lr)
-                optimized_weights_midpoint[initial_idx, target_idx] = opt_weights
-
-                adjacency_norm_opt = adjacency_norm - np.diag(opt_weights[-1, :])  # A_norm with optimized self-inhibition weights
-                if np.any(np.diag(adjacency_norm_opt) > 0):
-                    print('Found {0} positive diagonals on adjacency_norm_opt'.format(np.sum(np.diag(adjacency_norm_opt) > 0)))
-
-                # get the state trajectory and the control signals
-                state_trajectory, control_signals, numerical_error = get_control_inputs(A_norm=adjacency_norm_opt, x0=initial_state, xf=target_state,
-                                                                                        T=time_horizon, B=control_set,
-                                                                                        xr=reference_state, rho=rho, S=trajectory_constraints,
-                                                                                        system=system)
-
-                # integrate control signals to get control energy
-                node_energy = integrate_u(control_signals)
-                # summarize nodal energy
-                control_energy_midpoint[initial_idx, target_idx] = np.sum(node_energy)
-
-    # --- compute control energy. Reference state = zeros ---
-    print('Compute control energy. Reference state = zeros...')
-    optimized_weights_zero = np.zeros((n_states, n_states, n_steps, n_nodes))
-    control_energy_zero = np.zeros((n_states, n_states))
-    reference_state = 'zero'  # reference state
-    for initial_idx in tqdm(np.arange(n_states)):
-        initial_state = normalize_state(centroids[initial_idx, :])  # initial state
-        for target_idx in np.arange(n_states):
-            if initial_idx != target_idx:
-                target_state = normalize_state(centroids[target_idx, :])  # target state
-
-                loss, opt_weights = train_nct(adjacency_norm=adjacency_norm, initial_state=initial_state, target_state=target_state,
-                                              time_horizon=time_horizon, control_set=control_set,
-                                              reference_state=reference_state, rho=rho, trajectory_constraints=trajectory_constraints,
-                                              n_steps=n_steps, lr=lr)
-                optimized_weights_zero[initial_idx, target_idx] = opt_weights
-
-                adjacency_norm_opt = adjacency_norm - np.diag(opt_weights[-1, :])  # A_norm with optimized self-inhibition weights
-                if np.any(np.diag(adjacency_norm_opt) > 0):
-                    print('Found {0} positive diagonals on adjacency_norm_opt'.format(np.sum(np.diag(adjacency_norm_opt) > 0)))
-
-                # get the state trajectory and the control signals
-                state_trajectory, control_signals, numerical_error = get_control_inputs(A_norm=adjacency_norm_opt, x0=initial_state, xf=target_state,
-                                                                                        T=time_horizon, B=control_set,
-                                                                                        xr=reference_state, rho=rho, S=trajectory_constraints,
-                                                                                        system=system)
-
-                # integrate control signals to get control energy
-                node_energy = integrate_u(control_signals)
-                # summarize nodal energy
-                control_energy_zero[initial_idx, target_idx] = np.sum(node_energy)
-
-    # --- compute control energy. Reference state = fMRI clusters ---
-    # print('Compute control energy. Reference state = fMRI clusters...')
-    # control_energy_ref = np.zeros((n_states, n_states, n_states))
-    # optimized_weights = np.zeros((n_states, n_states, n_states, n_steps, n_nodes))
-    # for initial_idx in tqdm(np.arange(n_states)):
-    #     initial_state = normalize_state(centroids[initial_idx, :])  # initial state
-    #     for target_idx in np.arange(n_states):
-    #         if initial_idx != target_idx:
-    #             target_state = normalize_state(centroids[target_idx, :])  # target state
-    #             for reference_idx in np.arange(n_states):
-    #                 reference_state = normalize_state(centroids[reference_idx, :])  # reference state
-    #
-    #                 loss, opt_weights = train_nct(adjacency_norm=adjacency_norm, initial_state=initial_state, target_state=target_state,
-    #                                               time_horizon=time_horizon, control_set=control_set,
-    #                                               reference_state=reference_state, rho=rho, trajectory_constraints=trajectory_constraints,
-    #                                               n_steps=n_steps, lr=lr)
-    #
-    #                 adjacency_norm_opt = adjacency_norm - np.diag(opt_weights[-1, :])  # A_norm with optimized self-inhibition weights
-    #                 # print(optimized_weights[-1, :5], np.diag(adjacency_norm_opt[:5, :5]))
-    #
-    #                 # get the state trajectory and the control signals
-    #                 state_trajectory, control_signals, numerical_error = get_control_inputs(A_norm=adjacency_norm_opt, x0=initial_state, xf=target_state,
-    #                                                                                         T=time_horizon, B=control_set,
-    #                                                                                         xr=reference_state, rho=rho, S=trajectory_constraints,
-    #                                                                                         system=system)
-    #
-    #                 # integrate control signals to get control energy
-    #                 node_energy = integrate_u(control_signals)
-    #                 # summarize nodal energy
-    #                 control_energy_ref[initial_idx, target_idx, reference_idx] = np.sum(node_energy)
-    #                 optimized_weights[initial_idx, target_idx, reference_idx] = opt_weights
+                    # integrate control signals to get control energy
+                    node_energy = integrate_u(control_signals)
+                    # summarize nodal energy
+                    control_energy[initial_idx, target_idx] = np.sum(node_energy)
+    
+        log_args['loss_{0}'.format(reference_state)] = loss
+        log_args['eigen_values_{0}'.format(reference_state)] = eigen_values
+        log_args['optimized_weights_{0}'.format(reference_state)] = optimized_weights
+        log_args['control_energy_{0}'.format(reference_state)] = control_energy
 
     # save outputs
-    log_args = {
-        'control_energy_xf': control_energy_xf,
-        'optimized_weights_xf': optimized_weights_xf,
-        'control_energy_midpoint': control_energy_midpoint,
-        'optimized_weights_midpoint': optimized_weights_midpoint,
-        'control_energy_zero': control_energy_zero,
-        'optimized_weights_zero': optimized_weights_zero,
-        # 'control_energy_ref': control_energy_ref,
-    }
-    file_str = '{0}optimized_control_energy_k-{1}_T-{2}_c-{3}_rho-{4}'.format(file_prefix, n_states, time_horizon, c, rho)
+    file_str = '{0}optimized_control_energy_k-{1}_c-{2}_T-{3}_rho-{4}_nsteps-{5}_lr-{6}_eigweight-{7}_regweight-{8}'.format(file_prefix,
+                                                                                                                            n_states, 
+                                                                                                                            c, time_horizon, rho, 
+                                                                                                                            n_steps, lr, eig_weight, reg_weight)
     np.save(os.path.join(outdir, file_str), log_args)
 
     end = time.time()
@@ -210,7 +128,7 @@ def get_args():
     parser.add_argument('--indir', type=str, default='/home/lindenmp/research_projects/nct_xr/data')
     parser.add_argument('--outdir', type=str, default='/home/lindenmp/research_projects/nct_xr/results')
     parser.add_argument('--A_file', type=str, default='hcp_schaefer400-7_Am.npy')
-    parser.add_argument('--fmri_clusters_file', type=str, default='hcp_fmri_clusters_k-10.npy')
+    parser.add_argument('--fmri_clusters_file', type=str, default='hcp_fmri_clusters_k-5.npy')
     parser.add_argument('--file_prefix', type=str, default='hcp_')
     # parser.add_argument('--indir', type=str, default='/media/lindenmp/storage_ssd/research_projects/nct_xr/data/probabilistic_sift_radius2_count')
     # parser.add_argument('--outdir', type=str, default='/home/lindenmp/research_projects/nct_xr/results')
@@ -219,9 +137,13 @@ def get_args():
     # parser.add_argument('--file_prefix', type=str, default='pnc_')
 
     # settings
-    parser.add_argument('--time_horizon', type=int, default=1)
     parser.add_argument('--c', type=int, default=1)
+    parser.add_argument('--time_horizon', type=int, default=1)
     parser.add_argument('--rho', type=float, default=1)
+    parser.add_argument('--n_steps', type=int, default=1000)
+    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--eig_weight', type=float, default=0.001)
+    parser.add_argument('--reg_weight', type=float, default=0.001)
 
     args = parser.parse_args()
     args.indir = os.path.expanduser(args.indir)
@@ -241,9 +163,13 @@ if __name__ == '__main__':
         'file_prefix': args.file_prefix,
 
         # settings
-        'time_horizon': args.time_horizon,
         'c': args.c,
+        'time_horizon': args.time_horizon,
         'rho': args.rho,
+        'n_steps': args.n_steps,
+        'lr': args.lr,
+        'eig_weight': args.eig_weight,
+        'reg_weight': args.reg_weight,
     }
 
     run(config=config)
