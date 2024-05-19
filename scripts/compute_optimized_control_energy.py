d76c02e7564d9ca1958132a3b792b38453310e1d
lindenmp <lindenparkes@gmail.com>
2024-05-19 16:30:35 -0400

    add baseline control energy calculation for comparison. Also replaced ComputeControlEnergy with get_control_inputs

--- scripts/compute_optimized_control_energy.py
+++ scripts/compute_optimized_control_energy.py
@@ -34,9 +34,6 @@ def run(config):
     fmri_clusters_file = config['fmri_clusters_file']
     file_prefix = config['file_prefix']
 
-    # load A matrix
-    adjacency = np.load(os.path.join(indir, A_file))
-
     # load rsfMRI clusters
     fmri_clusters = np.load(os.path.join(outdir, fmri_clusters_file), allow_pickle=True).item()
     centroids = fmri_clusters['centroids']
@@ -50,29 +47,44 @@ def run(config):
     rho = config['rho']    
     control_set = np.eye(n_nodes)  # define control set using a uniform full control set
     trajectory_constraints = np.eye(n_nodes)  # define state trajectory constraints
-    print('nct params: c = {0}; time_horizon = {1}; rho = {2}'.format(c, time_horizon, rho))
+    print('Computing control energy.')
+    print('\tnct params: c = {0}; time_horizon = {1}; rho = {2}'.format(c, time_horizon, rho))
+
+    # load A matrix
+    adjacency = np.load(os.path.join(indir, A_file))
 
     # normalize adjacency matrix
     adjacency_norm = matrix_normalization(adjacency, system=system, c=c)
 
     # training params
+    reference_state = config['reference_state']
+    init_weights = config['init_weights']
     n_steps = config['n_steps']  # number of gradient steps
     lr = config['lr']  # learning rate for gradient
     eig_weight = config['eig_weight']  # regularization strength for eigen value penalty
     reg_weight = config['reg_weight']  # regularization strength for weight penalty (e.g., l2)
-    print('training params: n_steps = {0}; lr = {1}; eig_weight = {2}; reg_weight = {3}'.format(n_steps, lr, eig_weight, reg_weight))
+    reg_type = config['reg_type']  # regularization type: l1 or l2
+    print('\ttraining params: reference_state = {0}; init_weights = {1}; n_steps = {2}; lr = {3}; eig_weight = {4}; reg_weight = {5}; reg_type = {6}'.format(reference_state, init_weights, n_steps,
+                                                                                                                                                             lr, eig_weight, reg_weight, reg_type))
 
-    # compute control energy
-    start = time.time()
-    log_args = dict()
-    # for reference_state in ['zero',]:
-    for reference_state in ['zero', 'midpoint', 'xf']:
-        print('\ncomputing control energy. Reference state = {0}...'.format(reference_state))
+    file_str = '{0}optimized_control_energy_k-{1}_c-{2}_T-{3}_rho-{4}_refstate-{5}_initweights-{6}_nsteps-{7}_lr-{8}_eigweight-{9}_regweight-{10}_regtype-{11}'.format(file_prefix, 
+                                                                                                                                                                       n_states, 
+                                                                                                                                                                       c, time_horizon, rho,
+                                                                                                                                                                       reference_state, init_weights,
+                                                                                                                                                                       n_steps, lr, eig_weight, reg_weight, reg_type)
+
+    if os.path.isfile(os.path.join(outdir, file_str + '.npy')):
+        print('Output found. Skipping.')
+    else:
+        # compute control energy
+        start = time.time()
+        log_args = dict()
         loss = np.zeros((n_states, n_states, n_steps))
         eigen_values = np.zeros((n_states, n_states, n_steps))
         optimized_weights = np.zeros((n_states, n_states, n_steps, n_nodes))
-        control_energy = np.zeros((n_states, n_states))
-        
+        control_energy_variable_decay = np.zeros((n_states, n_states))
+        control_energy_static_decay = np.zeros((n_states, n_states))
+
         for initial_idx in np.arange(n_states):
             initial_state = normalize_state(centroids[initial_idx, :])  # initial state
             for target_idx in np.arange(n_states):
@@ -81,39 +93,48 @@ def run(config):
 
                 loss[initial_idx, target_idx], \
                 eigen_values[initial_idx, target_idx], \
-                optimized_weights[initial_idx, target_idx] = train_nct(adjacency_norm=adjacency_norm, initial_state=initial_state, target_state=target_state,
-                                                                        time_horizon=time_horizon, control_set=control_set,
-                                                                        reference_state=reference_state, rho=rho, trajectory_constraints=trajectory_constraints,
-                                                                        n_steps=n_steps, lr=lr, eig_weight=eig_weight, reg_weight=reg_weight)
-
-                idx = np.where(np.isnan(loss[initial_idx, target_idx]))[0][0] - 1
-                adjacency_norm_opt = adjacency_norm - np.diag(optimized_weights[initial_idx, target_idx, idx, :])  # A_norm with optimized self-inhibition weights
-
-                # get the state trajectory and the control signals
-                state_trajectory, control_signals, numerical_error = get_control_inputs(A_norm=adjacency_norm_opt, x0=initial_state, xf=target_state,
-                                                                                        T=time_horizon, B=control_set,
-                                                                                        xr=reference_state, rho=rho, S=trajectory_constraints,
-                                                                                        system=system)
-
+                optimized_weights[initial_idx, target_idx] = train_nct(adjacency_norm=adjacency_norm, initial_state=initial_state, target_state=target_state, time_horizon=time_horizon, control_set=control_set,
+                                                                    reference_state=reference_state, rho=rho, trajectory_constraints=trajectory_constraints, init_weights=init_weights,
+                                                                    n_steps=n_steps, lr=lr, eig_weight=eig_weight, reg_weight=reg_weight, reg_type=reg_type, early_stopping=True)
+                try:
+                    idx = np.where(np.isnan(loss[initial_idx, target_idx]))[0][0] - 1
+                except:
+                    idx = n_steps - 1
+                adjacency_weights = -1 - optimized_weights[initial_idx, target_idx, idx, :]
+                if np.any(adjacency_weights > 0):
+                    print('warning, positive weights found')
+                            
+                # get the state trajectory and the control signals with VARIABLE optimized self-inhibition weights
+                adjacency_norm_opt = adjacency_norm.copy()
+                adjacency_norm_opt[np.eye(n_nodes) == 1] = adjacency_weights
+                _, control_signals, _ = get_control_inputs(A_norm=adjacency_norm_opt, x0=initial_state, xf=target_state,
+                                                        T=time_horizon, B=control_set, xr=reference_state, rho=rho, S=trajectory_constraints, system=system)
                 # integrate control signals to get control energy
                 node_energy = integrate_u(control_signals)
                 # summarize nodal energy
-                control_energy[initial_idx, target_idx] = np.sum(node_energy)
-    
-        log_args['loss_{0}'.format(reference_state)] = loss
-        log_args['eigen_values_{0}'.format(reference_state)] = eigen_values
-        log_args['optimized_weights_{0}'.format(reference_state)] = optimized_weights
-        log_args['control_energy_{0}'.format(reference_state)] = control_energy
+                control_energy_variable_decay[initial_idx, target_idx] = np.sum(node_energy)
+                
+                # get the state trajectory and the control signals with STATIC optimized self-inhibition weights
+                adjacency_norm_opt = adjacency_norm.copy()
+                adjacency_norm_opt[np.eye(n_nodes) == 1] = np.mean(adjacency_weights)
+                _, control_signals, _ = get_control_inputs(A_norm=adjacency_norm_opt, x0=initial_state, xf=target_state,
+                                                        T=time_horizon, B=control_set, xr=reference_state, rho=rho, S=trajectory_constraints, system=system)
+                # integrate control signals to get control energy
+                node_energy = integrate_u(control_signals)
+                # summarize nodal energy
+                control_energy_static_decay[initial_idx, target_idx] = np.sum(node_energy)
+        
+            log_args['loss'] = loss
+            log_args['eigen_values'] = eigen_values
+            log_args['optimized_weights'] = optimized_weights
+            log_args['control_energy_variable_decay'] = control_energy_variable_decay
+            log_args['control_energy_static_decay'] = control_energy_static_decay
 
-    # save outputs
-    file_str = '{0}optimized_control_energy_k-{1}_c-{2}_T-{3}_rho-{4}_nsteps-{5}_lr-{6}_eigweight-{7}_regweight-{8}'.format(file_prefix,
-                                                                                                                            n_states, 
-                                                                                                                            c, time_horizon, rho, 
-                                                                                                                            n_steps, lr, eig_weight, reg_weight)
-    np.save(os.path.join(outdir, file_str), log_args)
+        # save outputs
+        np.save(os.path.join(outdir, file_str), log_args)
 
-    end = time.time()
-    print('...done in {:.2f} seconds.'.format(end - start))
+        end = time.time()
+        print('...done in {:.2f} seconds.'.format(end - start))
 
 # %%
 def get_args():
@@ -126,24 +147,22 @@ def get_args():
 
     parser.add_argument('--indir', type=str, default='/home/lindenmp/research_projects/nct_xr/data')
     parser.add_argument('--outdir', type=str, default='/home/lindenmp/research_projects/nct_xr/results')
-    parser.add_argument('--A_file', type=str, default='hcp_schaefer400-7_Am.npy')
+    parser.add_argument('--A_file', type=str, default='hcp_schaefer400-7_Am-features_schaefer_streamcount_areanorm_log.npy')
     parser.add_argument('--fmri_clusters_file', type=str, default='hcp_fmri_clusters_k-7.npy')
     parser.add_argument('--file_prefix', type=str, default='hcp_')
-    # parser.add_argument('--indir', type=str, default='/media/lindenmp/storage_ssd/research_projects/nct_xr/data/probabilistic_sift_radius2_count')
-    # parser.add_argument('--outdir', type=str, default='/home/lindenmp/research_projects/nct_xr/results')
-    # parser.add_argument('--A_file', type=str, default='pnc_schaefer400-7_Am.npy')
-    # parser.add_argument('--fmri_clusters_file', type=str, default='pnc_fmri_clusters_k-5.npy')
-    # parser.add_argument('--file_prefix', type=str, default='pnc_')
 
     # settings
     parser.add_argument('--c', type=int, default=1)
     parser.add_argument('--time_horizon', type=int, default=1)
     parser.add_argument('--rho', type=float, default=1)
-
+    
+    parser.add_argument('--reference_state', type=str, default='midpoint')
+    parser.add_argument('--init_weights', type=str, default='one')
     parser.add_argument('--n_steps', type=int, default=1000)
     parser.add_argument('--lr', type=float, default=0.01)
-    parser.add_argument('--eig_weight', type=float, default=0.001)
+    parser.add_argument('--eig_weight', type=float, default=0.1)
     parser.add_argument('--reg_weight', type=float, default=0.001)
+    parser.add_argument('--reg_type', type=str, default='l2')
 
     args = parser.parse_args()
     args.indir = os.path.expanduser(args.indir)
@@ -167,10 +186,13 @@ if __name__ == '__main__':
         'time_horizon': args.time_horizon,
         'rho': args.rho,
 
+        'reference_state': args.reference_state,
+        'init_weights': args.init_weights,
         'n_steps': args.n_steps,
         'lr': args.lr,
         'eig_weight': args.eig_weight,
         'reg_weight': args.reg_weight,
+        'reg_type': args.reg_type,
     }
 
     run(config=config)
