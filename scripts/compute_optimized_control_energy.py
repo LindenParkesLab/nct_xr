80146db817677b05aa58cda4de165c5e728ca2bc
Linden Parkes <lindenparkes@gmail.com>
2024-09-09 18:01:32 -0400

    clean up functions, moved to utils

--- scripts/compute_optimized_control_energy.py
+++ scripts/compute_optimized_control_energy.py
@@ -16,6 +16,7 @@ elif username == 'lp756':
 from nctpy.energies import get_control_inputs, integrate_u
 from nctpy.utils import normalize_state, matrix_normalization
 from src.neural_network import train_nct
+from src.utils import control_energy_helper, get_random_partial_control_set, get_yeo_control_set
 from snaplab_tools.utils import get_schaefer_system_mask
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -23,50 +24,6 @@ warnings.simplefilter(action='ignore', category=RuntimeWarning)
 warnings.simplefilter(action='ignore', category=UserWarning)
 
 # %%
-
-def control_energy_helper(inputs):
-    state_trajectory, control_signals, n_err = get_control_inputs(A_norm=inputs['adjacency_norm'], x0=inputs['initial_state'], xf=inputs['target_state'],
-                                                                  T=inputs['time_horizon'], B=inputs['control_set'], xr=inputs['reference_state'],
-                                                                  rho=inputs['rho'], S=inputs['trajectory_constraints'], system=inputs['system'])
-
-    # integrate control signals to get control energy
-    node_energy = integrate_u(control_signals)
-    # summarize nodal energy
-    control_energy = np.sum(node_energy)
-
-    costate_error = n_err[0]
-    xf_error = n_err[1]
- 
-    return state_trajectory, control_signals, node_energy, control_energy, costate_error, xf_error
-
-
-def get_random_partial_control_set(n_nodes, n_control_nodes, add_small_control=False, seed=0):
-    control_set = np.zeros((n_nodes, n_nodes))
-    if add_small_control:
-        control_set[np.eye(n_nodes) == 1] = 1e-5
-
-    np.random.seed(seed)
-    x = np.random.choice(np.arange(n_nodes), size=n_control_nodes, replace=False)
-    for i in x:
-        control_set[i, i] = 1
-        
-    return control_set
-
-
-def get_yeo_control_set(node_labels, system, add_small_control=False):
-    n_nodes = len(node_labels)
-    control_set = np.zeros((n_nodes, n_nodes))
-    if add_small_control:
-        control_set[np.eye(n_nodes) == 1] = 1e-7
-
-    system_mask = get_schaefer_system_mask(node_labels, system=system)
-    for i in np.arange(n_nodes):
-        if system_mask[i]:
-            control_set[i, i] = 1
-            
-    return control_set
-
-
 def run(config):
     outdir = config['outdir']
     A_file = config['A_file']
