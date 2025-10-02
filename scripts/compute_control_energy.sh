9bd8fd859f6935bdb398e0d1872ee4467da26073
Linden Parkes <lindenparkes@gmail.com>
2025-10-02 17:15:26 -0400

    clean up before release

--- scripts/compute_control_energy.sh
+++ scripts/compute_control_energy.sh
@@ -3,56 +3,18 @@
 projdir='/home/lindenmp/research_projects/nct_xr'
 scriptsdir=${projdir}'/scripts'
 indir=${projdir}'/data'
-outdir=${projdir}'/results'
-########################################################################################################################
-
-########################################################################################################################
 reference_state='xf'
+########################################################################################################################
 
-# main analysis: Average connectome; k=7 
-A_file=${indir}'/hcp_schaefer400-7_Am-features_schaefer_streamcount_areanorm_log.npy'
-fmri_clusters_file=${outdir}'/hcp_fmri_clusters_k-7.npy'
-file_prefix='hcp-Am'
+########################################################################################################################
+# main analysis: hcp average connectome; k=7 (RE-RUN DONE)
+outdir=${projdir}'/results/HCPYA'
+A_file=${indir}'/HCPYA_Schaefer4007_Am.npy'
+fmri_clusters_file=${outdir}'/HCPYA_Schaefer4007_rsts_fmri_clusters_k-7.npy'
+file_prefix='HCPYA-Schaefer4007-Am'
 
 python ${scriptsdir}/compute_optimized_control_energy.py --outdir ${outdir} \
 --A_file ${A_file} --fmri_clusters_file ${fmri_clusters_file} \
---file_prefix ${file_prefix} --reference_state ${reference_state} \
---run_rand_control_set 'True'
-########################################################################################################################
-
-########################################################################################################################
-outdir_replication=${outdir}'/replication'
-
-# replication analysis: Lausanne average connectome; k=7 
-A_file=${indir}'/lausanne_connectome.npy'
-fmri_clusters_file=${indir}'/lausanne_brain_states.npy'
-file_prefix='laus-Am'
-
-python ${scriptsdir}/compute_optimized_control_energy.py --outdir ${outdir_replication} \
---A_file ${A_file} --fmri_clusters_file ${fmri_clusters_file} \
 --file_prefix ${file_prefix} --reference_state ${reference_state}
 
-# replication analysis: MICA-MICs average connectome; k=7 
-A_file=${indir}'/mics_schaefer400-7_Am.npy'
-fmri_clusters_file=${outdir_replication}'/mics_fmri_clusters_k-7.npy'
-file_prefix='mics-Am'
-
-python ${scriptsdir}/compute_optimized_control_energy.py --outdir ${outdir_replication} \
---A_file ${A_file} --fmri_clusters_file ${fmri_clusters_file} \
---file_prefix ${file_prefix} --reference_state ${reference_state}
-########################################################################################################################
-
-########################################################################################################################
-# replication analysis: Average connectome; k=2-19
-A_file=${indir}'/hcp_schaefer400-7_Am-features_schaefer_streamcount_areanorm_log.npy'
-file_prefix='hcp-Am'
-
-for n_clusters in $(seq 2 14); do
-    fmri_clusters_file=${outdir_replication}'/hcp_fmri_clusters_k-'${n_clusters}'.npy'
-    echo ${fmri_clusters_file}
-
-    python ${scriptsdir}/compute_optimized_control_energy.py --outdir ${outdir_replication} \
-    --A_file ${A_file} --fmri_clusters_file ${fmri_clusters_file} \
-    --file_prefix ${file_prefix} --reference_state ${reference_state}
-done
 ########################################################################################################################
--- scripts/compute_fmri_clusters.sh
+++ scripts/compute_fmri_clusters.sh
@@ -3,19 +3,20 @@
 projdir='/home/lindenmp/research_projects/nct_xr'
 scriptsdir=${projdir}'/scripts'
 indir=${projdir}'/data'
-outdir=${projdir}'/results/replication'
 ########################################################################################################################
 
 ########################################################################################################################
 # main analysis: hcp data
+outdir=${projdir}'/results/HCPYA'
 for n_clusters in $(seq 2 20); do
-    python ${scriptsdir}/compute_fmri_clusters.py --indir ${indir} --outdir ${outdir} --fmri_file 'hcp_schaefer400-7_rsts.npy' --file_prefix 'hcp_' --n_clusters ${n_clusters}
+    python ${scriptsdir}/compute_fmri_clusters.py --indir ${indir} --outdir ${outdir} --fmri_file 'HCPYA_Schaefer4007_rsts.npy' --file_prefix 'HCPYA_Schaefer4007_rsts_' --n_clusters ${n_clusters}
 done
 ########################################################################################################################
 
 ########################################################################################################################
 # main analysis: mica-mics data
+outdir=${projdir}'/results/MICS'
 for n_clusters in $(seq 2 20); do
-    python ${scriptsdir}/compute_fmri_clusters.py --indir ${indir} --outdir ${outdir} --fmri_file 'mics_schaefer400-7_rsts.npy' --file_prefix 'mics_' --n_clusters ${n_clusters}
+    python ${scriptsdir}/compute_fmri_clusters.py --indir ${indir} --outdir ${outdir} --fmri_file 'MICS_Schaefer4007_rsts.npy' --file_prefix 'MICS_Schaefer4007_rsts_' --n_clusters ${n_clusters}
 done
 ########################################################################################################################
